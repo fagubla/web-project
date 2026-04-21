2108313509e4e9e8fc6240b82c820acb2fa87fbc
Fabio <frguachallab@gmail.com>
2026-04-21 16:56:12 -0400

    Possible test with TestSprite

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -47,7 +47,7 @@ const submit = () => {
                     <button type="submit">Submit</button>
                 </form>
             </div>
-            <div v-for="post in posts" :key="post.id"> {{ post.body }} {{ post.user.name }}</div>
+            <div v-for="post in posts" :key="post.id"> {{ post.body }} </div>
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
